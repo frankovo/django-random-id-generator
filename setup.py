2a2e5ca205a3660f604eaa41dd5e8b0d6161c922
frankovo <frankosehj22@gmail.com>
2018-04-10 00:14:34 +0200

    updates files

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-random-id-generator',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django app to generate unique random IDs.',
+    long_description=README,
+    url='https://github.com/frankovo/django-random-id-generator',
+    author='Joseph Oseh Frank',
+    author_email='frankosehj22@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
