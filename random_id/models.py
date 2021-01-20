03d4d0d0e26159de59f249aeb92f836ebda7c281
Joseph O. frank <frankosehj22@gmail.com>
2021-01-20 22:13:03 +0100

    update python_2_unicode_compatible import

--- random_id/models.py
+++ random_id/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 from random_id.utils import generate_random_string as id_generator
 
